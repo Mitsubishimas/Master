0e71ba8663c3cb454982abf8737c6d8e5ad92908
Mitsubishimas <mitsubishimas@github.com>
2026-06-16 16:21:03 +0000

    Master v2.5.0: Fixed splash crash, notification channel name

--- app/src/main/java/com/mastermitsu/cvt/SplashActivity.kt
+++ app/src/main/java/com/mastermitsu/cvt/SplashActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.animation.AlphaAnimation
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -12,17 +12,11 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         
-        val logo = findViewById<android.widget.ImageView>(android.R.id.content)
-        val fadeIn = AlphaAnimation(0f, 1f).apply {
-            duration = 1000
-            fillAfter = true
-        }
-        logo.startAnimation(fadeIn)
+        val logo = findViewById<ImageView>(R.id.splashLogo)
         
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         }, 2000)
     }
 }
